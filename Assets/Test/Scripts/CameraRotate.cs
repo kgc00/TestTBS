6fb7b5ddcd88c66e8ab6e602b6acdbd47687357e
Game Lab <patricia.l.umali@gmail.com>
2016-08-31 14:15:50 -0400

    First commit
    test first commit

---
+++ Assets/Test/Scripts/CameraRotate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour
+{
+
+    private CameraData cameraData;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Need the data container
+        cameraData = GetComponent<CameraData>();
+
+        if (cameraData == null)
+        {
+            cameraData = gameObject.AddComponent<CameraData>();
+        }
+
+        //Need the target in order to position relatively
+        cameraData.storeTarget();
+    }
+
+    void FixedUpdate()
+    {
+        if (cameraData.shouldRotateLeft())
+        {
+            cameraData.rotation = cameraData.rotation - 1;
+        }
+        else if (cameraData.shouldRotateRight())
+        {
+            cameraData.rotation = cameraData.rotation + 1;
+        }
+    }
+}
---
+++ Assets/TurnManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : MonoBehaviour {
+
+    public CellGrid CellGrid;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            //User ends his turn by pressing "n" on keyboard.
+            CellGrid.EndTurn();
+        }
+    }
+} 
